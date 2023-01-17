c7bb6188b73fab98096e2ea81f956ef22171e6e5
Crawron <azertyaaron@gmail.com>
2023-01-16 23:25:59 -0500

    basic site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: "'InterVariable', 'Inter', sans",
+			},
+			colors: {
+				accent: {
+					dark: "#D680FF",
+				},
+				paper: {
+					dark: "#1B1627",
+				},
+				ink: {
+					dark: "#CFDDFF",
+				},
+			},
+		},
 	},
 	plugins: [],
 }
